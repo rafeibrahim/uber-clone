624afca0abc8103788188c3b156e96e08a419681
Rafe Ibrahim <rafeibrahim@Rafes-MacBook-Pro.local>
2021-08-26 03:43:29 +0300

    first commit before react native elements

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
+import { store } from './store';
+//import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+
+// 1) Set up redux
+
+export default function App() {
+  return (
+    <Provider store={store}>
+        <HomeScreen /> 
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
